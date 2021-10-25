4866a8c24cef8a497d7c5e778f1464a33bb44a78
Brian Moniz <bmoniz328@gmail.com>
2021-10-25 14:36:26 -0700

    first commit

---
+++ Predict Destructuring/script.js
@@ -0,0 +1,11 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// The output will be Tesla first, then Mercedes second.
+
+
