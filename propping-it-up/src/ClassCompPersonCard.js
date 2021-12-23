adbdbc5d02dba1f3a9fe40706ffea099fc74cdb4
Brian Moniz <bmoniz328@gmail.com>
2021-12-22 16:53:53 -0800

    finished putting it together

--- propping-it-up/src/ClassCompPersonCard.js
+++ propping-it-up/src/ClassCompPersonCard.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 
-class ClassCompPersonCard extends Component {
-  // constructors are NOT needed when you are only using props
-  //    They come by default with inheritance from the Component class
-  // constructor(props) {
-  //   super(props);
-  // }
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      ageUp: this.props.age,
+    };
+  }
   render() {
-    const { firstName, lastName, age, hair } = this.props;
+    // age is used when setting up state and so we do not need to destructure it here!
+    const { firstName, lastName, hair } = this.props;
+
     return (
       <div className="container">
         <h2>
           {lastName}, {firstName}
         </h2>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.ageUp}</p>
         <p>Hair Color: {hair}</p>
+        <button onClick={() => this.setState({ ageUp: this.state.ageUp + 1 })}>
+          Birthday!!
+        </button>
       </div>
     );
   }
 }
-
-export default ClassCompPersonCard;
-
+export default PersonCard;
 
 // this is the class component way
